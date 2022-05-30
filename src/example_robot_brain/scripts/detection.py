239ede2ddad657fbab9994f43a0977a6a395edf0
Essen, Rick van <rick.vanessen@wur.nl>
2022-05-30 12:09:40 +0200

    make task mapping clearer

--- src/example_robot_brain/scripts/detection.py
+++ src/example_robot_brain/scripts/detection.py
@@ -2,12 +2,20 @@
 
 import numpy as np
 
+DETECTIONS = [
+    ["weed", np.array([1.055, 2.011])],
+    ["litter", np.array([2.357, 1.049])],
+    ["litter", np.array([2.497, 1.972])],
+    ["weed", np.array([2.217, 2.032])],
+]
+i = 0
+
 # Does not actually detect anything, just returns hardcoded locations
-def obj_mapper():
-    # detect weeds
-    weed_placements = np.array([[1, 2], [2, 2]])
+def detect():
+    global i
 
-    # detect weeds
-    litter_placements = np.array([[2, 1], [2, 1]])
+    i += 1
 
-    return weed_placements, litter_placements
+    # Just return one predefined weed position, we expect you to implement a better
+    # solution.
+    return DETECTIONS[(i - 1) % len(DETECTIONS)]
